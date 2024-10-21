2a625fd7857c2d527a669f6297619056cd021654
jugal1512 <jugallotwala@gmail.com>
2024-10-21 15:22:31 +0530

    operation on wishlist controller

---
+++ E-Mart.Domain/Wishlists/WishlistService.cs
@@ -0,0 +1,21 @@
+﻿using E_Mart.Domain.Base;
+
+namespace E_Mart.Domain.Wishlists;
+public class WishlistService : GenericService<wishlist>, IWishlistService
+{
+    private readonly IWishlistRepository _wishlistRepository;
+    public WishlistService(IWishlistRepository wishlistRepository) : base(wishlistRepository)
+    {
+        _wishlistRepository = wishlistRepository;
+    }
+
+    public async Task ClearWishlistAsync(int userId)
+    {
+        await _wishlistRepository.ClearWishlistAsync(userId);
+    }
+
+    public async Task<List<wishlist>> getwishListsByUserAsync(int userId)
+    {
+        return await _wishlistRepository.getwishListsByUserAsync(userId);
+    }
+}
---
+++ E-Mart.WebApi/Models/Wishlist/WishlistViewModal.cs
@@ -0,0 +1,17 @@
+﻿using E_Mart.WebApi.Models.Product;
+
+namespace E_Mart.WebApi.Models.Wishlist;
+
+public class WishlistViewModal
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int UserId { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool? IsDeleted { get; set; }
+    public bool IsActive { get; set; }
+    public virtual ProductViewModal Product { get; set; }
+
+}
